58254604ed444be449ca6d0c5741615e68ce68bf
Andrii Hurskyi <ahurs@softserveinc.com>
2020-10-08 10:01:47 +0300

    Add user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates :name, presence: true,
+                   length: { maximum: 50 }
+  validates :nickname, presence: true,
+                       uniqueness: true,
+                       format: { without: /\s/, message: "doesn't allow spaces" }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
