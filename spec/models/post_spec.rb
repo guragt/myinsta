12d100702eeb3dc2b1b63daa58cc868afc3b5700
Andrii Hurskyi <ahurs@softserveinc.com>
2020-10-22 15:08:58 +0300

    Add spec for Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:image) }
+    it { is_expected.to validate_length_of(:description).is_at_most(2000) }
+  end
 end
