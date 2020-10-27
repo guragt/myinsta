95933c32c1ac5c39fdceb68f88037a5e4ee7aac9
Andrii Hurskyi <ahurs@softserveinc.com>
2020-10-27 12:06:48 +0200

    Fix posts controller tests

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -5,48 +5,50 @@ RSpec.describe PostsController, type: :controller do
   let!(:post_valid_params) { attributes_for(:post)  }
   let!(:post_invalid_params) { { description: 'a' * 2001, image: '' } }
 
-  describe 'GET#new' do
-    context 'user is signed_in' do
-      before { sign_in user }
+  context 'authorized user' do
+    before { sign_in user }
+    
+    describe 'GET#new' do
       subject { get :new }
       it { is_expected.to have_http_status(:success) }
       it { is_expected.to render_template('new') }
     end
 
-    context 'user is not signed_in' do
+    describe 'POST#create' do  
+      context 'with valid params' do
+        it 'create a new post' do
+          expect do
+            post :create, params: { post: post_valid_params }
+          end.to change(Post, :count).by(1)
+          expect(response).to have_http_status(:redirect)
+          expect(flash[:success]).to be_present
+        end
+      end
+  
+      context 'with invalid params' do
+        it 'does not create a new post' do
+          expect do
+            post :create, params: { post: post_invalid_params }
+          end.not_to change(Post, :count)
+          expect(response).not_to have_http_status(:redirect)
+          expect(flash[:success]).not_to be_present
+        end
+      end
+    end
+  end
+
+  context 'unauthorized user' do
+    describe 'GET#new' do
       subject { get :new }
       it { is_expected.to have_http_status(:redirect) }
       it { is_expected.not_to render_template('new') }
     end
-  end
 
-  describe 'POST#create' do
-    before { sign_in user }
-
-    context 'with valid params' do
-      it 'create a new post' do
+    describe 'POST#create' do
+      it 'does not create a new post' do
         expect do
           post :create, params: { post: post_valid_params }
-        end.to change(Post, :count).by(1)
-      end
-
-      it 'redirect to home page' do
-        post :create, params: { post: post_valid_params }
-        expect(response).to have_http_status(:redirect)
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context 'with invalid params' do
-      it 'create a new post' do
-        expect do
-          post :create, params: { post: post_invalid_params }
         end.not_to change(Post, :count)
-      end
-
-      it 'redirect to home page' do
-        post :create, params: { post: post_invalid_params }
-        expect(response).not_to have_http_status(:redirect)
         expect(flash[:success]).not_to be_present
       end
     end
