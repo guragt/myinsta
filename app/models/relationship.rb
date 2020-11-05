1ca8d78956f438cd788f5c932537d2e9d8a7efa2
Andrii Hurskyi <ahurs@softserveinc.com>
2020-11-05 10:01:33 +0200

    Add Relationship model

---
+++ app/models/relationship.rb
@@ -0,0 +1,9 @@
+class Relationship < ApplicationRecord
+  enum status: { pending: 0, active: 1 }
+
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followed, class_name: 'User'
+
+  validates :follower_id, presence: true
+  validates :followed_id, presence: true
+end
