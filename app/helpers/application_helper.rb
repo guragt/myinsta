65d83f9a2c61019fad5306c0142ac36c2ab883b4
Andrii Hurskyi <ahurs@softserveinc.com>
2020-11-16 14:41:46 +0200

    helper method following_status_partial_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def following_status_partial_name(follower, followed)
+    case follower.following_status_for(followed)
+      when 'not_following' then 'follow'
+      when 'pending' then 'cancel'
+      when 'active' then 'unfollow'
+    end
+  end
 end
