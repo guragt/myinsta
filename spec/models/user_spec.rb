adbcc22bf780304f4cb45f8a35655cd541d5d24f
Andrii Hurskyi <ahurs@softserveinc.com>
2020-11-24 12:50:52 +0200

    Add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,23 @@ RSpec.describe User, type: :model do
       it { is_expected.to validate_uniqueness_of(:nickname) }
     end
   end
+
+  describe 'Following status' do
+    let!(:user) { create(:user) }
+    let!(:other_user) { create(:user) }
+
+    it 'should retun status not_following' do
+      expect(user.following_status_for(other_user)).to eq('not_following')
+    end
+
+    it 'should retun status pending' do
+      user.following << other_user
+      expect(user.following_status_for(other_user)).to eq('pending')
+    end
+
+    it 'should retun status active' do
+      user.active_relationships.create(followed_id: other_user.id, status: 'active')
+      expect(user.following_status_for(other_user)).to eq('active')
+    end
+  end
 end
