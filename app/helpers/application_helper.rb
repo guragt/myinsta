6a6a5c73e7b455936518f9ba8978bee6549ac08a
Andrii Hurskyi <ahurs@softserveinc.com>
2020-11-27 11:43:28 +0200

    Likes for comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,6 +10,6 @@ module ApplicationHelper
   def likes_count_block(likes_count)
     return if likes_count.zero?
 
-    tag.span(t('posts.like_form.like', count: likes_count), class: 'post-like-count')
+    tag.span(t('posts.like_form.like', count: likes_count), class: 'like-count')
   end
 end
