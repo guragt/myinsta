86208048585e4d3ffc8a7a0516908ff13acf9d18
Andrii Hurskyi <ahurs@softserveinc.com>
2021-04-01 17:18:36 +0300

    Changes in db for okta

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_121839) do
+ActiveRecord::Schema.define(version: 2021_04_01_135033) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "body"
@@ -58,6 +58,15 @@ ActiveRecord::Schema.define(version: 2020_12_29_121839) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
+  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
@@ -72,10 +81,14 @@ ActiveRecord::Schema.define(version: 2020_12_29_121839) do
     t.datetime "deleted_at"
     t.boolean "private", default: false
     t.string "type", default: "User"
+    t.string "provider"
+    t.string "uid"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["nickname"], name: "index_users_on_nickname", unique: true
+    t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid"], name: "index_users_on_uid"
   end
 
   add_foreign_key "comments", "users"
