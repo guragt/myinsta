9c93a93d077926b7079d2a7655dec12c9ec1981e
Andrii Hurskyi <ahurs@softserveinc.com>
2021-04-02 12:07:25 +0300

    Fix okta

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
-gem 'activerecord-session_store'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'bootstrap-sass', '3.4.1'
 gem 'carrierwave', '~> 2.0'
--- db/schema.rb
+++ db/schema.rb
@@ -58,15 +58,6 @@ ActiveRecord::Schema.define(version: 2021_04_01_135033) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.text "data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "nickname"
