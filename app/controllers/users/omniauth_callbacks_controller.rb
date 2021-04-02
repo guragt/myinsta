8074257a13556b1c22c3048f334ad3da96e63ef3
Andrii Hurskyi <ahurs@softserveinc.com>
2021-04-02 14:49:12 +0300

    Add warning for not registered users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,12 @@ module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def oktaoauth
       @user = User.from_omniauth(request.env['omniauth.auth'])
-      sign_in_and_redirect @user
+      if @user.persisted?
+        sign_in_and_redirect @user
+      else
+        flash[:warning] = t('need_sign_up')
+        redirect_to new_user_registration_path
+      end
     end
   end
 end
