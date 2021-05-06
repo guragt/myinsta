dfaa04a683d08e30ae390684b493a04e018f91e6
Andrii Hurskyi <ahurs@softserveinc.com>
2021-05-06 09:38:16 +0300

    Hello Vue

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -5,17 +5,17 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+// import Vue from 'vue'
+// import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     render: h => h(App)
+//   }).$mount()
+//   document.body.appendChild(app.$el)
 
-  console.log(app)
-})
+//   console.log(app)
+// })
 
 
 // The above code uses Vue without the compiler, which means you cannot
@@ -26,25 +26,20 @@ document.addEventListener('DOMContentLoaded', () => {
 // Add <%= javascript_pack_tag 'hello_vue' %> to your layout
 // Then add this markup to your html template:
 //
-// <div id='hello'>
-//   {{message}}
-//   <app></app>
-// </div>
 
+import Vue from 'vue/dist/vue.esm'
+import App from '../app.vue'
 
-// import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//   const app = new Vue({
-//     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
-//     },
-//     components: { App }
-//   })
-// })
-//
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    el: '#main',
+    data: {
+      message: "Can you say hello?"
+    },
+    components: { App }
+  })
+})
+
 //
 //
 // If the project is using turbolinks, install 'vue-turbolinks':
@@ -56,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
 // import TurbolinksAdapter from 'vue-turbolinks'
 // import Vue from 'vue/dist/vue.esm'
 // import App from '../app.vue'
-//
+
 // Vue.use(TurbolinksAdapter)
-//
+
 // document.addEventListener('turbolinks:load', () => {
 //   const app = new Vue({
 //     el: '#hello',
