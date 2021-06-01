1f8aede861fd9149df2e06cc269a5194eb0db187
Andrii Hurskyi <ahurs@softserveinc.com>
2021-06-01 16:59:32 +0300

    Add tests for new okta user flow

--- spec/controllers/users/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/users/omniauth_callbacks_controller_spec.rb
@@ -2,21 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Users::OmniauthCallbacksController, type: :controller do
   let!(:existing_user) { create(:user, provider: 'oktaoauth', uid: '12345') }
+
   let!(:existing_user_okta_hash) do
     OmniAuth::AuthHash.new({
                              provider: existing_user.provider,
                              uid: existing_user.uid,
                              info: {
-                               email: existing_user.email
+                               email: existing_user.email,
+                               name: 'Existing User'
+                             },
+                             extra: {
+                               raw_info: {
+                                 nickname: 'existing_user'
+                               }
                              }
                            })
   end
-  let!(:new_user_okta_hash) do
+
+  let!(:new_valid_user_okta_hash) do
     OmniAuth::AuthHash.new({
                              provider: 'oktaoauth',
-                             uid: '67890',
+                             uid: '12346',
                              info: {
-                               email: 'new_user@test.net'
+                               email: 'new_user@test.net',
+                               name: 'New User'
+                             },
+                             extra: {
+                               raw_info: {
+                                 nickname: 'new_user'
+                               }
+                             }
+                           })
+  end
+
+  let!(:new_invalid_user_okta_hash) do
+    OmniAuth::AuthHash.new({
+                             provider: 'oktaoauth',
+                             uid: '12347',
+                             info: {
+                               email: 'new_user@test.net',
+                               name: ''
+                             },
+                             extra: {
+                               raw_info: {
+                                 nickname: ''
+                               }
                              }
                            })
   end
@@ -29,7 +59,6 @@ RSpec.describe Users::OmniauthCallbacksController, type: :controller do
     context 'existing user' do
       before do
         request.env['omniauth.auth'] = existing_user_okta_hash
-
         get :oktaoauth
       end
 
@@ -42,19 +71,35 @@ RSpec.describe Users::OmniauthCallbacksController, type: :controller do
       end
     end
 
-    context 'new user' do
+    context 'new valid user' do
       before do
-        request.env['omniauth.auth'] = new_user_okta_hash
+        request.env['omniauth.auth'] = new_valid_user_okta_hash
 
         get :oktaoauth
       end
 
       it 'redirect to root_path' do
-        expect(response).to redirect_to(edit_profile_users_path)
+        expect(response).to redirect_to(root_path)
       end
 
       it 'should have a current_user' do
         expect(subject.current_user).to_not eq(nil)
+      end
+    end
+
+    context 'new invalid user' do
+      before do
+        request.env['omniauth.auth'] = new_invalid_user_okta_hash
+
+        get :oktaoauth
+      end
+
+      it 'redirect to root_path' do
+        expect(response).to render_template('new')
+      end
+
+      it 'should have a current_user' do
+        expect(subject.current_user).to eq(nil)
       end
     end
   end
